528838232e90ce602e4a72c3f5dcb6f3c4fa89ad
H k <helikhatri64@gmail.com>
2020-11-25 19:55:43 +0530

    Importing Project

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TrafficSignalSystem.MvcApplication" Language="C#" %>
---
+++ Models/clsCommon.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TrafficSignalSystem.Models
+{
+    public class clsCommon
+    {
+        public int intDurationId { get; set; }
+        public int intSignalRotation { get; set; }
+        public string strRotationType { get; set; }
+    }
+}
